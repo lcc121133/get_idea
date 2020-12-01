14c91ffee34e8e7c0aecdb2009ca1064d264a1f6
lc <1584237186@qq.com>
2020-12-01 20:40:21 +0800

    这是第一次

---
+++ src/main/java/com/baizhi/GetIdeaApplication.java
@@ -0,0 +1,12 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetIdeaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GetIdeaApplication.class, args);
+	}
+
+}
